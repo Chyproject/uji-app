1805e4bf58378013d106dd8c72711a5321e3038b
Chyproject <cahyoedysaputra@gmail.com>
2024-05-25 22:02:49 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,16 @@
 <html>
-    <head>
-        <title>
-            App Name -@yield('title')
-        </title>
-    </head>
-    <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
-        <div class ="container">
-            @yield('content')
-        </div>
-    </body>
+
+<head>
+    <title>App Name - @yield('title')</title>
+</head>
+
+<body>
+    @section('sidebar')
+    This is the master sidebar.
+    @endsection
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,41 @@
 <?php
 
+use App\Http\Controllers\RakBukuController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginRegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/buku', function () {
-    $data =[];
-    $data['poin'] = 83;
-    $data['flag'] = '2';
-    $data ['sub_judul']= 'latihan parsing data di view';
-    $data ['buku']= ['buku 1', 'buku 2', 'buku 3', 'buku 4', 'buku 5'];
-    return view('buku/list', $data);
-});
-Route::view('/dashboard','dashboard');
 
+Route::get('/biodata', function () {
+    return view('biodata');
+});
+
+Route::resource('rak_buku', RakBukuController::class);
+
+// Register & Login
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+    });
+
+    // Ajax Rak Buku
+Route::post('/rak_buku/ajax_store',[RakBukuController::class,
+'ajax_store']);
 
