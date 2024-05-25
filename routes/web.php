9851e1c0327b0c3f908b3e9eb9f8dfdd3b2959ce
Chyproject <cahyoedysaputra@gmail.com>
2024-05-25 21:43:42 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/buku', function () {
+    $data =[];
+    $data['poin'] = 83;
+    $data['flag'] = '2';
+    $data ['sub_judul']= 'latihan parsing data di view';
+    $data ['buku']= ['buku 1', 'buku 2', 'buku 3', 'buku 4', 'buku 5'];
+    return view('buku/list', $data);
+});
+Route::view('/dashboard','dashboard');
+
+
